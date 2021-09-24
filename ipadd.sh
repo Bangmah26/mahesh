361aa4be66d893b14873282736e9e59a6d78a0d7
Bangmah26 <Bangmah26@outlook.com>
2021-09-24 07:46:24 +0000

    changes to ipadd.sh

--- ipadd.sh
+++ ipadd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo sed -i '/azurevm/d' /etc/ansible/hosts
-az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null
+az login --service-principal -u=$ARM_CLIENT_ID -p=$ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null
 ip=`az vm list-ip-addresses -n myVM -g proj1-rg --query [].virtualMachine.network.publicIpAddresses[0].ipAddress -o tsv`
 echo -e "azurevm ansible_host=$ip ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" | sudo tee -a /etc/ansible/hosts > /dev/null
 
